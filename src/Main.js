1b870432b8b8e221411da8cd52497711114c9fde
ELDUZ <mamedovelduz1@gmail.com>
2023-07-26 12:40:13 +0400

    -

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Main.css'
+
+function Main() {
+  return (
+    <div className="body">
+      <div className="filterDiv"></div>
+      <div className="productDiv"></div>
+    </div>
+  )
+}
+
+export default Main
