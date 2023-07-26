8ceacf429490a8ec9b6ac0d609ce984bab889b49
ELDUZ <mamedovelduz1@gmail.com>
2023-07-26 13:42:02 +0400

    -

--- src/Main.js
+++ src/Main.js
@@ -4,8 +4,17 @@ import './Main.css'
 function Main() {
   return (
     <div className="body">
-      <div className="filterDiv"></div>
-      <div className="productDiv"></div>
+      <div className="filterDiv">
+        <div><b className='sectionText'>Filters</b></div>
+        <div className='filterText'></div>
+        <div className='filterSize'></div>
+        <div className='filterPrice'></div>
+      </div>
+      <div className="productDiv">
+        <div className='item'>
+          <div><img src='images/1.jpg'></img></div>
+        </div>
+      </div>
     </div>
   )
 }
